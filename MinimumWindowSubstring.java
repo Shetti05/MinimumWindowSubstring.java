a449ae6cef327c2728b0d9770b071ec6c89f86d0
jaideep shetti <Jaideepshetti55@gmail.com>
2026-02-14 19:10:25 +0530

    first commmit

---
+++ MinimumWindowSubstring.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class MinimumWindowSubstring {
+
+    public static String minWindow(String s, String t) {
+        if (s.length() < t.length()) return "";
+
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : t.toCharArray())
+            map.put(c, map.getOrDefault(c, 0) + 1);
+
+        int left = 0, right = 0, count = t.length();
+        int minLen = Integer.MAX_VALUE, start = 0;
+
+        while (right < s.length()) {
+            char r = s.charAt(right);
+            if (map.containsKey(r)) {
+                if (map.get(r) > 0) count--;
+                map.put(r, map.get(r) - 1);
+            }
+            right++;
+
+            while (count == 0) {
+                if (right - left < minLen) {
+                    minLen = right - left;
+                    start = left;
+                }
+
+                char l = s.charAt(left);
+                if (map.containsKey(l)) {
+                    map.put(l, map.get(l) + 1);
+                    if (map.get(l) > 0) count++;
+                }
+                left++;
+            }
+        }
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start + minLen);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(minWindow("ADOBECODEBANC", "ABC"));
+    }
+}
